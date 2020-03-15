22f7b7ea556a67b25881b299655af56e6b0573b9
Anikesh348 <44468436+Anikesh348@users.noreply.github.com>
2020-03-15 12:43:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-$('.carousel').carousel({
-  interval: 6000,
-  pause: "false"
+$('.small').click(function() {
+  $(".discover a").removeClass("size1");
 });
+
+if ($(window).width() > 700 && $(window).width() < 1350) {
+  swal("Please use our site in desktop or in mobile phones");
+}
